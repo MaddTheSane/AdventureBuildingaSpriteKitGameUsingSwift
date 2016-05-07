0f21ced43ec1ead1bf463be2c90063efba8f1bf7
C.W. Betts <computers57@hotmail.com>
2016-05-07 00:49:07 -0600

    A more recent version of Swift Adventure.

--- Adventure/Adventure Shared/Sprites/EnemyCharacter.swift
+++ Adventure/Adventure Shared/Sprites/EnemyCharacter.swift
@@ -1,18 +1,31 @@
 /*
-  Copyright (C) 2014 Apple Inc. All Rights Reserved.
+  Copyright (C) 2015 Apple Inc. All Rights Reserved.
   See LICENSE.txt for this sample’s licensing information
   
   Abstract:
-  
-        Defines the class for enemy characters
-      
+  Defines the class for enemy characters
 */
 
 import SpriteKit
 
 class EnemyCharacter: Character {
+    // MARK: Properties
+    
     // Subclasses always set the intelligence in their initializers.
-    var intelligence: ArtificialIntelligence! = nil
+    var intelligence: ArtificialIntelligence!
+    
+    class var deathSplort: SKSpriteNode {
+        get {
+            return SharedSprites.sprites[characterType]?[SharedSprites.Keys.deathSplort] ?? SKSpriteNode()
+        }
+        set {
+            var spritesForCharacterType = SharedSprites.sprites[characterType] ?? [String: SKSpriteNode]()
+            spritesForCharacterType[SharedSprites.Keys.deathSplort] = newValue
+            SharedSprites.sprites[characterType] = spritesForCharacterType
+        }
+    }
+    
+    // MARK: Scene Processing Support
 
     override func updateWithTimeSinceLastUpdate(interval: NSTimeInterval) {
         super.updateWithTimeSinceLastUpdate(interval)
@@ -22,7 +35,7 @@ class EnemyCharacter: Character {
 
     override func animationDidComplete(animationState: AnimationState) {
         if animationState == AnimationState.Attack {
-            intelligence.target?.collidedWith(physicsBody)
+            intelligence.target?.collidedWith(physicsBody!)
         }
     }
 }
